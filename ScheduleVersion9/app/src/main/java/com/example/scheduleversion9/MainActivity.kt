167a1dec16bd11a2cb3e76bcec0ec80db85a42f7
JoshuaHGeorge <jhgeorg1@lakeheadu.ca>
2020-04-04 13:12:14 -0400

    Re-added firebase connection

--- ScheduleVersion9/app/src/main/java/com/example/scheduleversion9/MainActivity.kt
+++ ScheduleVersion9/app/src/main/java/com/example/scheduleversion9/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.scheduleversion9
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
